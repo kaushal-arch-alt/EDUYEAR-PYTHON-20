787f74cc6f51a581054b478342b58c42ce2178ee
kaushal-arch-alt <72266740+kaushal-arch-alt@users.noreply.github.com>
2021-05-05 16:32:32 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,12 @@
+a=58
+b=20.56
+c="Kaushal"
+
+print("{} is an integer.".format(a))
+print("{} is a float number.".format(b))
+print("{} is a string".format(c))
+print("Type Coversions:")
+print("Coversion of integer to string:",str(a))
+print("Coversion of float to string:",str(b))
+print("Coversion of float to integer:",int(b))
+print("Coversion of integer to float:",float(a))
